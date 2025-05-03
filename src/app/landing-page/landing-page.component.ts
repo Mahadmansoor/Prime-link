e3b78429f1f1277a9791c30afefa222c83cb2264
mahadmansoorkb <mahad.mansoor@kikbits.com>
2025-05-03 19:10:35 +0500

    initial changes

---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, HostListener } from '@angular/core';
+import { CommonModule, NgClass } from '@angular/common';
+import { RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.css'
+})
+export class LandingPageComponent implements OnInit {
+
+  ngOnInit(): void {}
+}
