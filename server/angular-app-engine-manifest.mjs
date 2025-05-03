e12568c5af05f69211763fdaa53851813f2d0c92
mahadmansoorkb <mahad.mansoor@kikbits.com>
2025-05-03 20:03:36 +0500

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'https://Mahadmansoor.github.io/Prime-link',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
