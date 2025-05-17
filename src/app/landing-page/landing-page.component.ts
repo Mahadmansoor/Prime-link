9ea234097b5ab03bbccdd819df60be8bb29342ae
mahadmansoorkb <mahad.mansoor@kikbits.com>
2025-05-17 16:02:21 +0500

    UI fixes

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, Inject, OnInit, PLATFORM_ID } from '@angular/
 import { isPlatformBrowser } from '@angular/common';
 import { CommonModule, NgClass } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-landing-page',
   standalone: true,
@@ -17,5 +18,17 @@ export class LandingPageComponent {
 
   closeSidebar(): void {
     this.isSidebarOpen = false;
+  }
+
+  scrollToTop(): void {
+    // Prevent default anchor behavior
+    event?.preventDefault();
+    
+    // Scroll to top with smooth behavior
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
   }
 }
