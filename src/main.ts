0b211762e21ca6997d4ee583d6ba955713e8be0c
mahadmansoorkb <mahad.mansoor@kikbits.com>
2025-06-11 14:26:49 +0500

    performance issues solved

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { routes } from './app/app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes),
-    provideHttpClient()
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideHttpClient(withFetch())
   ]
 }).catch(err => console.error(err));
